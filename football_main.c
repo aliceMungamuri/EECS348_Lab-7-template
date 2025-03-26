eb343c6f46298a0f0e52f89b8373551bc8ff4cc2
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2025-03-26 09:22:54 -0500

    Update football_main.c

--- football_main.c
+++ football_main.c
@@ -1,5 +1,20 @@
 #include "football.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main() {
+    int score;
+
+    while (1) {
+        printf("Enter the NFL score (0 or 1 to STOP): ");
+        scanf("%d", &score);
+
+        if (score <= 1) {
+            printf("Exiting program.\n");
+            break;
+        }
+
+        print_combinations(score);
+    }
+
     return 0;
 }
