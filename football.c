50da59c694cde77ba28e702435cc8cd9554b1b63
aliceMungamuri <158488797+aliceMungamuri@users.noreply.github.com>
2025-03-26 22:46:02 -0500

    Update football.c

--- football.c
+++ football.c
@@ -13,12 +13,12 @@
 void print_combinations(int points) {
     printf("Possible combinations of scoring plays if a team’s score is %d:\n", points);
 
-    for (int td8 = 0; td8 * TD_8 <= points; td8++) {
-        for (int td7 = 0; td7 * TD_7 + td8 * TD_8 <= points; td7++) {
-            for (int td6 = 0; td6 * TD_6 + td7 * TD_7 + td8 * TD_8 <= points; td6++) {
-                for (int fg = 0; fg * FG_3 + td6 * TD_6 + td7 * TD_7 + td8 * TD_8 <= points; fg++) {
-                    for (int safety = 0; safety * SAFETY_2 + fg * FG_3 + td6 * TD_6 + td7 * TD_7 + td8 * TD_8 <= points; safety++) {
-                        if (td8 * TD_8 + td7 * TD_7 + td6 * TD_6 + fg * FG_3 + safety * SAFETY_2 == points) {
+    for (int td8 = 0; td8 * Touchdown8 <= points; td8++) {
+        for (int td7 = 0; td7 * Touchdown7 + td8 * Touchdown8 <= points; td7++) {
+            for (int td6 = 0; td6 * Touchdown6 + td7 * Touchdown7 + td8 * Touchdown8 <= points; td6++) {
+                for (int fg = 0; fg * Fieldgoal3 + td6 * Touchdown6 + td7 * Touchdown7 + td8 * Touchdown8 <= points; fg++) {
+                    for (int safety = 0; safety * Safety2 + fg * Fieldgoal3 + td6 * Touchdown6 + td7 * Touchdown7 + td8 * Touchdown8 <= points; safety++) {
+                        if (td8 * Touchdown8 + td7 * Touchdown7 + td6 * Touchdown6 + fg * Fieldgoal3 + safety * Safety2 == points) {
                             printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n",
                                    td8, td7, td6, fg, safety);
                         }
